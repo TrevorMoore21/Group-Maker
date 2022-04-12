337800ba52b13ada0ec93f6bd99c371e2b16f88d
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2022-04-12 13:40:45 -0600

    importing file

--- GroupMaker/src/GroupMaker.java
+++ GroupMaker/src/GroupMaker.java
@@ -1,4 +1,9 @@
-
+import java.util.Scanner;
+import java.io.File;
+//import java.io.FileNotFoundException;
+import java.io.IOException;
 public class GroupMaker {
-
+	public static void main(String[] args) throws IOException {
+		Scanner myFile = new Scanner(new File("StudentGroupList.txt"));
+	}
 }
