65a635aa00010cd79c50514e497a6c9d9a27faac
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2022-04-20 13:46:09 -0600

    pp

---
+++ GroupMaker/src/WillitWork.java
@@ -0,0 +1,26 @@
+
+public class WillitWork
+	{
+
+		static int txtsize = 2;
+		static int counter = 1;
+		static boolean maxSize = true;
+		
+		public static void willitWorkQmark()
+		{
+			
+			while (maxSize = true)
+				{
+					int nOfGroups = txtsize / counter;
+					int rotation = (((counter - 1) *2) + 1);
+			
+					if (nOfGroups > rotation)
+						{
+							counter++;
+							maxSize = false;
+						}
+				}
+			
+		}
+		
+	}
